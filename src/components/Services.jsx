cef3c8c0d7eab03c1f67ca4c75904cecab5f1cf3
Jhonatan <jhonatanjau98@gmail.com>
2025-08-18 20:34:10 -0300

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -72,10 +72,10 @@ const Services = () => {
       {/* Modal de Visualização */}
       {selectedVideo && (
         <div
-          className="fixed inset-0 bg-black/90 flex items-center justify-center z-50 p-4"
+          className="fixed inset-0 bg-black/90 flex items-center justify-center z-50"
           onClick={() => setSelectedVideo(null)}
         >
-          <div className="relative w-full max-w-4xl">
+          <div className="relative w-full max-w-4xl mx-4">
             <video
               src={selectedVideo}
               controls
