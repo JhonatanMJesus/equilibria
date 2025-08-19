cb628b36720d16183e54b505acce7e059c2c4169
Jhonatan <jhonatanjau98@gmail.com>
2025-08-18 23:05:06 -0300

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -75,7 +75,7 @@ const Services = () => {
       {/* Modal */}
       {selectedVideo && (
         <div
-          className="fixed inset-0 bg-black/50 flex justify-center items-center z-50 p-4"
+          className="inset-0 bg-black/50 flex justify-center items-center z-50 p-4"
           onClick={() => setSelectedVideo(null)}
         >
           {/* Contêiner que envolve apenas o vídeo */}
